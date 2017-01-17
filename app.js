93cd694df345b32bfe9539ef9839ae16e7b02939
vturman <logical@nxt.ru>
2017-01-18 01:07:02 +0300

    test 0.1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(123);
+var express = require('express');
+var router = express.Router();
+
+var app = express();
+
+app.set('port', 3000);
+app.set('ip', "0.0.0.0");
+
+app.use('/', router);
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html'); 
+});
+
+var server = app.listen(app.get('port'), app.get('ip'), function () {
+    console.log('Wi-Fi MODULE listening on: ' + app.get('ip') + ':' + app.get('port'));
+});
