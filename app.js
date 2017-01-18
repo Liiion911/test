ff8fec0a1554bf2563a137474b84a214c7f7a565
vturman <logical@nxt.ru>
2017-01-18 22:39:15 +0300

    no message

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 var express = require('express');
-var router = express.Router();
-
+var session = require('express-session');
 var app = express();
+
+app.set('trust proxy', 1) // trust first proxy 
+app.use(session({
+  secret: '1234567890QWERTYaa',
+  resave: true,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}));
 
 app.set('port', 3000);
 app.set('ip', "0.0.0.0");
 
-app.use('/', router);
+var maxPings = 60;
+var pingCode = '994359278';
 
-/* GET home page. */
-router.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html'); 
+app.get('/', function (req, res) {
+	if (req.query.callback && req.query.ping){
+		console.log(req.query.ping + '; ' + req.session.ping);
+		if(!req.session.ping || req.query.ping <= (parseInt(req.session.ping) + 1)){
+			req.session.ping = parseInt(req.query.ping);
+			res.jsonp({ test: 1 });
+		}
+	} else if (req.query.callback && req.query.final){
+		console.log(req.query.final + '; ' + req.session.ping);
+		if(req.session.ping && req.session.ping >= maxPings - 1 && req.query.final <= req.session.ping + 1){
+			res.jsonp({ code: pingCode });
+		}
+	} else {
+		res.send(' ');
+	}
 });
 
 var server = app.listen(app.get('port'), app.get('ip'), function () {
